7947d460349e13bdb088731e777332ceba52052b
AbdulMoiz8994 <moiza8994@gmail.com>
2021-10-20 16:05:40 +0500

    add get the html struture via queryselector

--- Project5/script.js
+++ Project5/script.js
@@ -1,1 +1,20 @@
+//the  get all html via class and id queryselector() 
+let form=document.querySelector('.formAuth')
+let userName=document.querySelector('.username')
+let email=document.querySelector('#email')
+let password=document.querySelector('#password')
+let number=document.querySelector('#number')
+let confirmPassword=document.querySelector('#confirmPassword')
+
+
+console.log(form);
+console.log(userName);
+console.log(email);
+console.log(password);
+console.log(number);
+console.log(confirmPassword);
+
+
+
+// make the function via addEventListencer which accept to things
 
