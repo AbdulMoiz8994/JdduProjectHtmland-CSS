05dad060a8fd11498e2e86cb0c011337a3363616
AbdulMoiz8994 <moiza8994@gmail.com>
2021-10-20 17:57:42 +0500

    I add full JS logic in script js

--- Project5/script.js
+++ Project5/script.js
@@ -16,5 +16,74 @@ console.log(confirmPassword);
 
 
 
+
 // make the function via addEventListencer which accept to things
+function showError(input, message){
+// from parentElement we get the paret's Tag
+let formContorl=input.parentElement;
+// console.log(formContorl, message);
+
+ const smallTag=formContorl.querySelector('small')
+//    console.log(smallTag);
+   smallTag.innerHTML=message
+}
+function showSuccess(input){
+    // same thing here
+    let formContorl=input.parentElement
+    // console.log(formContorl);
+     
+    const smallTag=formContorl.querySelector('small')
+    // console.log(smallTag);
+
+}
+form.addEventListener('submit',(e) =>{
+    // for avoding re-rendering to on submit 
+           e.preventDefault();
+
+//    after runing this function we need to again empty the input box
+
+
+// conditions
+if(userName.value === ''){
+    // console.log(userName.value);
+    showError(userName,'Please write username')
+     
+}else{
+    showSuccess(userName)
+}
+userName.value=""
+
+if(email.value === ''){
+    showError(email,'Please write email')
+}else{
+    showSuccess(email)
+}
+email.value=""
+
+if(number.value === ''){
+    showError(number,'Please write number')
+}else{
+    showSuccess(number)
+}
+number.value=""
+if(password.value === ''){
+    showError(password,'Please write Password')
+}else{
+    showSuccess(password)
+}
+password.value=""
+if(confirmPassword.value === ''){
+    showError(confirmPassword,'Please write correct Password')
+}else{
+    showSuccess(confirmPassword)
+}
+confirmPassword.value=""
+
+if(password.value !== confirmPassword.value){
+    alert('Please write correct Password')
+}else{
+    alert('Form Has Submitted')
+}
+
+})
 
