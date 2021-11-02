895db174290e4acf774abd72224893bde9ad6427
AbdulMoiz8994 <moiza8994@gmail.com>
2021-11-03 00:47:20 +0500

    Add the selction of box active class and remove class as well and score calculate as well

--- Project15/main-quiz.js
+++ Project15/main-quiz.js
@@ -54,7 +54,8 @@ let questions=[
 
 // make the counter of question 
 let questionNum=0;
-
+// score calculate
+let score=0;
 
 // calling function onload
 var load=document.querySelector('body')
@@ -66,12 +67,52 @@ function question(e){
     //calling dom element of question
     const questionele=document.getElementById('questionElement')
     // console.log(questionele);
-    questionele.innerHTML=questions[0].question
+    questionele.innerHTML=questions[e].question
     // get the dom elements for options
     const options=document.getElementsByClassName('optionElement')
     // console.log(options);
     for(var i=0; i < options.length; i++){
-        options[i].innerHTML=questions[0].option[i]
+        options[i].innerHTML=questions[e].option[i]
     }
 
+}
+
+function nextQuestion(){
+    rightanswer(questionNum)    
+    questionNum++;
+    removeactiveclass()
+     question(questionNum)
+    //  console.log(questions.length);
+//    if(questions.length >=4){
+//        alert("Show Result")
+//    }
+
+}
+
+
+function putactiveclass(e){
+    // console.log(e);  
+    removeactiveclass()
+    e.classList.add('active')
+}
+
+function removeactiveclass(){
+    // get that element whihc active class applied
+    const activeclass=document.getElementsByClassName('active')   
+    // console.log(activeclass);
+    // the could be applied on more than one options so we put the for loop 
+    for(var i=0; i< activeclass.length; i++){
+        // console.log(activeclass[0]);
+    activeclass[i].classList.remove('active')            
+    }
+}
+function rightanswer(e){
+    const activeclass=document.getElementsByClassName('active')   
+    // console.log(activeclass[0].innerHTML);
+    for(var i=0; i < activeclass.length; i++){
+        if(activeclass[i].innerHTML == questions[e].answer){
+            console.log(score +=10);
+        }
+    }
+    
 }
