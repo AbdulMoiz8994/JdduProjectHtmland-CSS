03876ee3d7b9538e5d0ee0319618205b85a0a160
AbdulMoiz8994 <moiza8994@gmail.com>
2021-11-02 19:29:09 +0500

    add the index.jhtml page and scipt.js where I amke the input box and when the user write his of her name it will redirect to quiz.html page where I am making th whole quiz in main-quiz.js

---
+++ Project15/main-quiz.js
@@ -0,0 +1,52 @@
+// get the dom element
+let mainDiv=document.querySelector('.question_cont')
+
+// make the list of question ain array of objects
+let questions=[
+    {
+        question: 'Q1 CPU Stands for?',
+        answer: "central processing unit",
+        option:[
+            "central processing unit",
+             "cental programming unit",
+             "central performance Unit",
+             "central per unit"
+        ]
+    },
+    {
+        question: 'Q2 The term "Computer" is the derived from ?',
+        answer: "latin",
+        option:[
+            "latin",
+             "english",
+             "german",
+             "Arabic"
+        ]
+    },
+    {
+        question: 'Q3 Who is the father of Computer ?',
+        answer: "Charles Babbage",
+        option:[
+            "Augusta Adaming",
+             "Charles babbage",
+        ]
+    },
+    {
+        question: 'Q4 WWW Stands for ?',
+        answer: "World Wide Web",
+        option:[
+            "World Whole Web",
+             "Wide World Web",
+             "Web World Wide",
+             "World Wide Web"
+        ]
+    },
+    {
+        question: 'Q5 Who is the father of Internet ?',
+        answer: "World Wide Web",
+        option:[
+            "Vint Cerf",
+             "Denis Riche",
+        ]
+    },
+]
